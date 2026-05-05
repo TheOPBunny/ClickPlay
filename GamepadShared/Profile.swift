4d572175f6417a5ad5edc842239b07b96c8697f1
TheOPBunny <hassanzahid1999@gmail.com>
2026-05-04 21:43:32 -0400

    Add modifier recording and key labels

--- GamepadShared/Profile.swift
+++ GamepadShared/Profile.swift
@@ -779,16 +779,141 @@ extension ButtonConfig {
         return parts.joined()
     }
 
-    private static func keyName(_ code: Int) -> String {
+    static func isModifierKey(code: Int) -> Bool {
+        modifierFlag(forKeyCode: code) != nil
+    }
+
+    static func modifierFlag(forKeyCode code: Int) -> NSEvent.ModifierFlags? {
+        switch code {
+        case 54, 55:
+            return .command
+        case 56, 60:
+            return .shift
+        case 57:
+            return .capsLock
+        case 58, 61:
+            return .option
+        case 59, 62:
+            return .control
+        case 63:
+            return .function
+        default:
+            return nil
+        }
+    }
+
+    static func keyName(_ code: Int) -> String {
         let keyNames: [Int: String] = [
-            0: "A", 1: "S", 2: "D", 3: "F", 4: "H", 5: "G", 6: "Z", 7: "X", 8: "C", 9: "V",
-            11: "B", 12: "Q", 13: "W", 14: "E", 15: "R", 16: "Y", 17: "T", 31: "O", 32: "U",
-            34: "I", 35: "P", 37: "L", 38: "J", 40: "K", 45: "N", 46: "M",
-            36: "↩", 48: "⇥", 49: "Space", 51: "⌫", 53: "⎋",
-            123: "←", 124: "→", 125: "↓", 126: "↑",
-            96: "F5", 97: "F6", 98: "F7", 99: "F3", 100: "F8", 101: "F9",
-            103: "F11", 109: "F10", 111: "F12",
-            115: "Home", 116: "PgUp", 117: "Del", 119: "End", 121: "PgDn",
+            0: "A",
+            1: "S",
+            2: "D",
+            3: "F",
+            4: "H",
+            5: "G",
+            6: "Z",
+            7: "X",
+            8: "C",
+            9: "V",
+            11: "B",
+            12: "Q",
+            13: "W",
+            14: "E",
+            15: "R",
+            16: "Y",
+            17: "T",
+            18: "1",
+            19: "2",
+            20: "3",
+            21: "4",
+            22: "6",
+            23: "5",
+            24: "=",
+            25: "9",
+            26: "7",
+            27: "-",
+            28: "8",
+            29: "0",
+            30: "]",
+            31: "O",
+            32: "U",
+            33: "[",
+            34: "I",
+            35: "P",
+            36: "Return",
+            37: "L",
+            38: "J",
+            39: "'",
+            40: "K",
+            41: ";",
+            42: "\\",
+            43: ",",
+            44: "/",
+            45: "N",
+            46: "M",
+            47: ".",
+            48: "Tab",
+            49: "Space",
+            50: "`",
+            51: "Delete",
+            53: "Esc",
+            54: "R⌘",
+            55: "⌘",
+            56: "Shift",
+            57: "Caps Lock",
+            58: "Option",
+            59: "Control",
+            60: "R Shift",
+            61: "R Option",
+            62: "R Control",
+            63: "Fn",
+            64: "F17",
+            65: "Num .",
+            67: "Num *",
+            69: "Num +",
+            71: "Num Clear",
+            75: "Num /",
+            76: "Enter",
+            78: "Num -",
+            79: "F18",
+            80: "F19",
+            81: "Num =",
+            82: "Num 0",
+            83: "Num 1",
+            84: "Num 2",
+            85: "Num 3",
+            86: "Num 4",
+            87: "Num 5",
+            88: "Num 6",
+            89: "Num 7",
+            90: "F20",
+            91: "Num 8",
+            92: "Num 9",
+            96: "F5",
+            97: "F6",
+            98: "F7",
+            99: "F3",
+            100: "F8",
+            101: "F9",
+            103: "F11",
+            105: "F13",
+            106: "F16",
+            107: "F14",
+            109: "F10",
+            111: "F12",
+            113: "F15",
+            114: "Help",
+            115: "Home",
+            116: "Page Up",
+            117: "Forward Delete",
+            118: "F4",
+            119: "End",
+            120: "F2",
+            121: "Page Down",
+            122: "F1",
+            123: "Left",
+            124: "Right",
+            125: "Down",
+            126: "Up",
         ]
 
         return keyNames[code] ?? "key(\(code))"
